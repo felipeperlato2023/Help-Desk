0aed5ecd2422ee897dc8ed45b94457a7c479fc5b
Felipe Perlato <fe.perlato@gmail.com>
2024-01-07 19:28:56 -0300

    padrao DTO para tecnico

--- src/main/java/com/felipe/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/felipe/helpdesk/resources/TecnicoResource.java
@@ -1,5 +1,7 @@
 package com.felipe.helpdesk.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felipe.helpdesk.domain.Tecnico;
+import com.felipe.helpdesk.domain.dtos.TecnicoDTO;
 import com.felipe.helpdesk.services.TecnicoService;
 
 @RestController
@@ -17,12 +20,20 @@ public class TecnicoResource {
 	@Autowired
 	TecnicoService service;
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id)
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id)
 	{
 		Tecnico obj = service.findById(id);
 		
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
+	
+	@GetMapping
+	public ResponseEntity<List<Tecnico>> findAll(){
+	     
+		List<Tecnico> list = service.findAll(); 
+		
+		return ResponseEntity.ok().body(list);
+}
 	
 	
 }
--- src/main/java/com/felipe/helpdesk/services/TecnicoService.java
+++ src/main/java/com/felipe/helpdesk/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.felipe.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,10 @@ public class TecnicoService {
 		Optional<Tecnico> obj = repository.findById(id);
 		
 		return obj.orElse(null);
+	}
+	
+	public List<Tecnico> findAll()
+	{
+		return repository.findAll();
 	}
 }
