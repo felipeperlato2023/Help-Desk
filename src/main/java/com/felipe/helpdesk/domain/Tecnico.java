d78e1f3b3d6beed8482c5d847d1949b8a81e5066
Felipe Perlato <fe.perlato@gmail.com>
2024-01-10 10:41:38 -0300

    create para tecnico

--- src/main/java/com/felipe/helpdesk/domain/Tecnico.java
+++ src/main/java/com/felipe/helpdesk/domain/Tecnico.java
@@ -2,8 +2,10 @@ package com.felipe.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.felipe.helpdesk.domain.dtos.TecnicoDTO;
 import com.felipe.helpdesk.domain.enums.Perfil;
 
 import jakarta.persistence.Entity;
@@ -25,6 +27,16 @@ public class Tecnico extends Pessoa {
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.TECNICO);
+	}
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfil = obj.getPerfil().stream().map(x -> x.getCode()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
