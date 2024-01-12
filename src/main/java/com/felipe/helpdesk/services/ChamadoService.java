7f559e6529f9ccb86d921a6bf2ea826e6021a57f
Felipe Perlato <fe.perlato@gmail.com>
2024-01-12 16:04:52 -0300

    findById para chamados

---
+++ src/main/java/com/felipe/helpdesk/services/ChamadoService.java
@@ -0,0 +1,23 @@
+package com.felipe.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipe.helpdesk.domain.Chamado;
+import com.felipe.helpdesk.repositories.ChamadoRepository;
+import com.felipe.helpdesk.services.exception.ObjectnotFoundException;
+
+@Service
+public class ChamadoService {
+
+	@Autowired
+	private ChamadoRepository repository;
+
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectnotFoundException("objeto não encontrado! ID: " + id));
+	}
+}
