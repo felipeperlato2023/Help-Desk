a28421d7a62028bc0d77b0d3013772a4684cf43e
Felipe Perlato <fe.perlato@gmail.com>
2024-01-12 21:32:54 -0300

    update chamado

--- src/main/java/com/felipe/helpdesk/services/ChamadoService.java
+++ src/main/java/com/felipe/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.felipe.helpdesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,12 +16,13 @@ import com.felipe.helpdesk.domain.enums.Status;
 import com.felipe.helpdesk.repositories.ChamadoRepository;
 import com.felipe.helpdesk.services.exception.ObjectnotFoundException;
 
+import jakarta.validation.Valid;
+
 @Service
 public class ChamadoService {
 
 	@Autowired
 	private ChamadoRepository repository;
-	
 	@Autowired
 	private TecnicoService tecnicoService;
 	@Autowired
@@ -31,28 +33,36 @@ public class ChamadoService {
 
 		return obj.orElseThrow(() -> new ObjectnotFoundException("objeto não encontrado! ID: " + id));
 	}
-	
+
 	public List<Chamado> findAll() {
 		return repository.findAll();
 	}
-	
-	
 
 	public Chamado create(ChamadoDTO objDTO) {
-		
+
 		return repository.save(newChamado(objDTO));
 	}
-	
+
+	public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+		objDTO.setId(id);
+		Chamado oldObj = findById(id);
+		oldObj = newChamado(objDTO);
+		return repository.save(oldObj);
+	}
+
 	private Chamado newChamado(ChamadoDTO obj) {
 		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
 		Cliente cliente = clienteService.findById(obj.getCliente());
-		
+
 		Chamado chamado = new Chamado();
-		if(obj.getId() != null)
-		{
+		if (obj.getId() != null) {
 			chamado.setId(obj.getId());
 		}
-		
+
+		if (obj.getStatus().equals(2)) {
+			chamado.setDataFechamento(LocalDate.now());
+		}
+
 		chamado.setTecnico(tecnico);
 		chamado.setCliente(cliente);
 		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
@@ -60,9 +70,7 @@ public class ChamadoService {
 		chamado.setTitulo(obj.getTitulo());
 		chamado.setObervacoes(obj.getObervacoes());
 		return chamado;
-		
-		
+
 	}
-	
-	
+
 }
